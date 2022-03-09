1063c902f1aa62fadf21b6df001cd3745f841945
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-09 22:09:41 +0530

    Ganesh Adding Last name .cs file for UC2

---
+++ UserLastName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class UserLastName
+    {
+        public string Regex_Pattern = "^[A-Z]{1}[a-z]{3,}$";
+
+        public bool validateLastName (string UserLastName)
+        {
+            return Regex.IsMatch(UserLastName, Regex_Pattern);
+        }
+    }
+}
