ca483625f4c88a19984c06b529b8e8d5f562bb5f
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-09 23:18:09 +0530

    Ganesh Adding final Password file for After UC5 to UC7

---
+++ PasswordRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class PasswordRules
+    {
+        public static string Regex_Pattern = "^[A-Z]{1}[a-z]{3}[@]{1}[0-9]{3}$";
+        public bool ValidatePass(string password)
+        {
+            return Regex.IsMatch(password, Regex_Pattern);
+        }
+    }
+}
