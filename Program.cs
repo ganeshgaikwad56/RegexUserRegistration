5dacecb63c600f546df82084f358ad9209ddf535
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-09 22:07:17 +0530

    Ganesh Adding Program.cs file for UC2

--- Program.cs
+++ Program.cs
@@ -8,16 +8,15 @@ namespace RegexDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome User Registration Account Program File");
-            //UserRegistration registration = new UserRegistration();
-            //Console.WriteLine("Enter Your Name");
-            //string value = Console.ReadLine();
-            //bool match = registration.ValidateName(value);
+            UserLastName registration = new UserLastName();
+            Console.WriteLine("Enter Your Last Name");
+            string value = Console.ReadLine();
+            bool match = registration.validateLastName(value);
 
-            //if (match)
-            //    Console.WriteLine("Name Matched with given criteria");
-            //else
-            //    Console.WriteLine("Pattern not Matched with given criteria should first name start with Capital and has min 3 charecters.");
+            if (match)
+                Console.WriteLine("Last Name Matched with given criteria");
+            else
+                Console.WriteLine("Pattern not Matched with given criteria,should Last name start with Capital and has min 3 charecters.");
 
         }
     }
