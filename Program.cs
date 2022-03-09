84fe7a07f827e4359244a3a0bcb95ae21c5547f5
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-09 21:41:17 +0530

    Ganesh_pushing greeting message

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using RegexDemo;
+using System;
+
+namespace RegexDemo
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome User Registration Account Program File");
+            //UserRegistration registration = new UserRegistration();
+            //Console.WriteLine("Enter Your Name");
+            //string value = Console.ReadLine();
+            //bool match = registration.ValidateName(value);
+
+            //if (match)
+            //    Console.WriteLine("Name Matched with given criteria");
+            //else
+            //    Console.WriteLine("Pattern not Matched with given criteria should first name start with Capital and has min 3 charecters.");
+
+        }
+    }
+}
