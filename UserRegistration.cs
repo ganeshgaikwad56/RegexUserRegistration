39de339b4c27f5bddc419d75627fc4e25fbd1277
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-09 21:49:08 +0530

    Ganesh adding first Name file for UC1

---
+++ UserRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class UserRegistration
+    {
+        public static string Regex_Pattern = "^[A-Z]{1}[a-z]{3,10}$";
+        public bool ValidateName (string Name)
+        {
+            return Regex.IsMatch(Name, Regex_Pattern);
+        }
+    }
+}
