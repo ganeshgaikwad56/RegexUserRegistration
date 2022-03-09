f3f1c94bd42c43ad81588b435979d9b9b9b29de8
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-09 22:45:58 +0530

    Ganesh Adding Program.cs and mobile number file for UC4

---
+++ Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Pattern
+    {
+        public static string Regex_Pattern = @"^(91[' '])[7-9]{1}[0-9]{9}$";
+
+        public bool ValidateMobileNumber(string number)
+        {
+            return Regex.IsMatch(number, Regex_Pattern);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,16 +8,15 @@ namespace RegexDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome User Registration Account Program File");
-            //UserRegistration registration = new UserRegistration();
-            //Console.WriteLine("Enter Your Name");
-            //string value = Console.ReadLine();
-            //bool match = registration.ValidateName(value);
+            Pattern registration = new Pattern();
+            Console.WriteLine("Enter Your mobile Number");
+            string value = Console.ReadLine();
+            bool match = registration.ValidateMobileNumber(value);
 
-            //if (match)
-            //    Console.WriteLine("Name Matched with given criteria");
-            //else
-            //    Console.WriteLine("Pattern not Matched with given criteria should first name start with Capital and has min 3 charecters.");
+            if (match)
+                Console.WriteLine("Mobile Number Matched with given criteria");
+            else
+                Console.WriteLine("Pattern of Number not Matched with given criteria");
 
         }
     }
