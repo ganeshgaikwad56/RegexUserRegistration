36fc93fb2dcf516d1133f7ef4ed674caca915690
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-09 21:46:23 +0530

    Ganesh pushing Program.cs file for UC1

--- Program.cs
+++ Program.cs
@@ -8,16 +8,15 @@ namespace RegexDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome User Registration Account Program File");
-            //UserRegistration registration = new UserRegistration();
-            //Console.WriteLine("Enter Your Name");
-            //string value = Console.ReadLine();
-            //bool match = registration.ValidateName(value);
+            UserRegistration registration = new UserRegistration();
+            Console.WriteLine("Enter Your Name");
+            string value = Console.ReadLine();
+            bool match = registration.ValidateName(value);
 
-            //if (match)
-            //    Console.WriteLine("Name Matched with given criteria");
-            //else
-            //    Console.WriteLine("Pattern not Matched with given criteria should first name start with Capital and has min 3 charecters.");
+            if (match)
+                Console.WriteLine("Name Matched with given criteria");
+            else
+                Console.WriteLine("Pattern not Matched with given criteria should first name start with Capital and has min 3 charecters.");
 
         }
     }
