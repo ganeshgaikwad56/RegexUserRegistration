f1c8afc8c6fc4b49fe61666d95b2dc257355134a
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-09 22:35:24 +0530

    Ganesh Adding Program.cs and EmailID file for UC3

---
+++ EmailID.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class EmailID
+    {
+        public string Regex_Pattern = "^[abc]{3}[.]{1}[a-z]{3}[@]{1}[bl]{2}[.]{1}[co]{2}[.]{1}[a-z]{2,}$";
+        public bool ValidateEmail(string Email)
+        {
+            return Regex.IsMatch(Email, Regex_Pattern);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,16 +8,15 @@ namespace RegexDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome User Registration Account Program File");
-            //UserRegistration registration = new UserRegistration();
-            //Console.WriteLine("Enter Your Name");
-            //string value = Console.ReadLine();
-            //bool match = registration.ValidateName(value);
+            EmailID registration = new EmailID();
+            Console.WriteLine("Enter Your EmailID");
+            string value = Console.ReadLine();
+            bool match = registration.ValidateEmail(value);
 
-            //if (match)
-            //    Console.WriteLine("Name Matched with given criteria");
-            //else
-            //    Console.WriteLine("Pattern not Matched with given criteria should first name start with Capital and has min 3 charecters.");
+            if (match)
+                Console.WriteLine("EmailID Matched with given criteria");
+            else
+                Console.WriteLine("Pattern not Matched with given criteria.Retry");
 
         }
     }
