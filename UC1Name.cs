33db11cefae6ca9412173898b7f3dd1ba3003766
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-20 15:27:49 +0530

    Ganesh Adding Program.cs file for uc 10

---
+++ UC1Name.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LambdaDemo
+{
+    public class UC1Name
+    {
+       public string FirstName = "^[A-Z][a-z]{2,}$";
+        public string FirstnameCheckusingLambda(string firstName) => Regex.IsMatch(firstName, FirstName) ? "FirstName is Valid" : "FirstName is not Valid";
+        
+    }
+
+}
+   
